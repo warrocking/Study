f507779ea02fda37a66cac65aaac04283ac97e3a
warrocking <115214928+warrocking@users.noreply.github.com>
2026-02-11 16:01:33 +0900

    Normalize layout and track only C sources

---
+++ C/Basic/0211_ex_17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    int n = 5;
+    printf("%d\n", factorial(n));
+    return 0;
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Industry_04
